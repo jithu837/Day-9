69a709e2847bfb09064e906bfdf6cb586b4bf7e6
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-10 14:19:44 +0530

    Add files via upload

---
+++ ForEachLoopString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class  ForEachLoopString
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a[]={"Apple","Orange","Pineapple"};
+        for(String b:a)
+        System.out.print(b+",");
+    }
+}
